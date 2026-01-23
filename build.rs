979fbf547da0157061a93bb4a5217620e325f71e
Jalon Wong <jalonwong@gmail.com>
2026-01-22 18:59:54 -0600

    Upgrade FreeRTOS

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
+use freertos_build::prelude::*;
+
 fn main() {
     let mut b = freertos_build::Builder::new();
-    b.user_config_dir("src_c");
+    b.cpu_clock(72.MHz());
+    b.heap_size(16 * 1024);
+    b.use_preemption(true);
+    b.max_task_priorities(5);
+    b.interrupt_priority_bits(4, 5, 15);
+    b.minimal_stack_size(40);
+    b.max_task_name_len(8);
     b.compile().unwrap();
 }
